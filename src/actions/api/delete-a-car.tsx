22900b5ded4b74be7475a887b2617fbffb26eae0
Eliveuton Souza <eliveuton3m@hotmail.com>
2024-08-05 19:00:12 -0300

    feat: delete a car

---
+++ src/actions/api/delete-a-car.tsx
@@ -0,0 +1,21 @@
+"use server";
+
+import getLoggedInUser from "./get-logged-in-user";
+import db from "@/lib/db";
+
+export default async function DeleteACar(carId: string) {
+  try {
+    const user = await getLoggedInUser();
+
+    console.log("entrei em deletar carro", carId);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    await db.carMaintenance.deleteMany({ where: { carId: carId } });
+    await db.car.deleteMany({ where: { id: carId } });
+  } catch (error) {
+    console.error(error);
+  }
+}
