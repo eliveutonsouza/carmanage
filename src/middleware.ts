4a04ae7c5ab0162d3b41a9a1fb14aa92519704af
Eliveuton Souza <eliveuton3m@hotmail.com>
2024-08-16 01:42:47 -0300

    fix: correction in cookie handling in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,18 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("session-token")?.value;
+  // Obtém todos os cookies como uma matriz de pares chave-valor
+  const cookiesArray = request.cookies.getAll();
+
+  // Encontra o cookie cujo nome termina com '.session-token'
+  const sessionTokenCookie = cookiesArray.find(({ name }) =>
+    /\.session-token$/.test(name)
+  );
+
+  // Se o cookie for encontrado, obtenha seu valor
+  const token = sessionTokenCookie ? sessionTokenCookie.value : null;
+
+  console.log(token);
 
   if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", request.url));
