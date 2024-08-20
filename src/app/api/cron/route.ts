aeb5a03514519b0131704315af22033de54a92a2
Eliveuton Souza <eliveuton3m@hotmail.com>
2024-08-20 19:43:51 -0300

    fix: cron job send all user

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,4 +1,3 @@
-import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function GET(request: NextRequest) {
@@ -12,30 +11,29 @@ export function GET(request: NextRequest) {
   return Response.json({ success: true });
 }
 
-export async function POST() {
-  try {
-    const response = await fetch("/api/mails/send-all-user", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-    });
+// export async function POST() {
+//   try {
+//     const response = await fetch("/api/mails/send-all-user", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//     });
 
-    // Checks if the response was successful
-    if (!response.ok) {
-      throw new Error(`Request error: ${response.statusText}`);
-    }
+//     if (!response.ok) {
+//       throw new Error(`Request error: ${response.statusText}`);
+//     }
 
-    return NextResponse.json(
-      { message: "Expired maintenance emails sent to users" },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      {
-        message: "Error sending expired maintenance emails to users",
-        error: (error as Error).message,
-      },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(
+//       { message: "Expired maintenance emails sent to users" },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json(
+//       {
+//         message: "Error sending expired maintenance emails to users",
+//         error: (error as Error).message,
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
