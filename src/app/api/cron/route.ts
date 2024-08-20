83e55e8958bc0e887e4b1cd8917078363d83a5f1
Eliveuton Souza <eliveuton3m@hotmail.com>
2024-08-20 20:03:46 -0300

    fix: cron job

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,39 +1,26 @@
-import type { NextRequest } from "next/server";
+import type { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest) {
-  const authHeader = request.headers.get("authorization");
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response("Unauthorized", {
-      status: 401,
+export async function GET(request: NextRequest, response: NextResponse) {
+  try {
+    const authHeader = request.headers.get("authorization");
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    }
+
+    const apiResponse = await fetch("/api/mails/send-all-user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
     });
-  }
 
-  return Response.json({ success: true });
+    if (!apiResponse.ok) {
+      throw new Error(`Request error: ${apiResponse.statusText}`);
+    }
+  } catch (error) {
+    console.error(error);
+    return response.json();
+  }
 }
 
-// export async function POST() {
-//   try {
-//     const response = await fetch("/api/mails/send-all-user", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//     });
-
-//     if (!response.ok) {
-//       throw new Error(`Request error: ${response.statusText}`);
-//     }
-
-//     return NextResponse.json(
-//       { message: "Expired maintenance emails sent to users" },
-//       { status: 200 }
-//     );
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json(
-//       {
-//         message: "Error sending expired maintenance emails to users",
-//         error: (error as Error).message,
-//       },
-//       { status: 500 }
-//     );
-//   }
-// }
+export async function POST() {}
