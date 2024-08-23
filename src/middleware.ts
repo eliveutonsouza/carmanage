12b6568f1179115084d1d9c831ae4e78e15a17bd
Eliveuton Souza <eliveuton3m@hotmail.com>
2024-08-23 15:18:58 -0300

    fix: middleware for accept connections externals for router api/ingest

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,38 +2,53 @@
 
 import { NextResponse, NextRequest } from "next/server";
 
+// Função middleware que processa as requisições
 export function middleware(request: NextRequest) {
+  // Obtém todos os cookies da requisição
   const cookiesArray = request.cookies.getAll();
+  // Busca o cookie de sessão
   const sessionTokenCookie = cookiesArray.find(({ name }) =>
     /\.session-token$/.test(name)
   );
+  // Define o token como o valor do cookie de sessão ou null se não existir
   const token = sessionTokenCookie ? sessionTokenCookie.value : null;
 
-  // Permitir acesso ao endpoint do cron job
-  if (request.nextUrl.pathname.startsWith("/api/cron/")) {
-    return NextResponse.next();
+  // Função para verificar permissões de acesso
+  const isPublicPath = (path: string) => {
+    // Define rotas públicas que não requerem autenticação
+    return path.startsWith("/api/cron/") || path.startsWith("/api/inngest/");
+  };
+
+  // Permitir acesso a rotas públicas
+  if (isPublicPath(request.nextUrl.pathname)) {
+    return NextResponse.next(); // Continua a requisição
   }
 
+  // Redireciona para login se não houver token e a rota for dashboard
   if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
+  // Redireciona para dashboard se houver token e a rota for login
   if (!!token && request.nextUrl.pathname.startsWith("/login")) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
+  // Redireciona para dashboard se houver token e a rota for registro
   if (!!token && request.nextUrl.pathname.startsWith("/register")) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
+  // Continua a requisição se nenhuma das condições acima for atendida
   return NextResponse.next();
 }
 
+// Configuração do middleware para as rotas que ele deve interceptar
 export const config = {
   matcher: [
-    "/login:path*",
-    "/dashboard/:path*",
-    "/register/:path*",
-    "/api/cron/:path*",
+    "/login:path*", // Rota de login
+    "/dashboard/:path*", // Rota de dashboard
+    "/register/:path*", // Rota de registro
+    "/api/cron/:path*", // Rota de API cron
   ],
 };
